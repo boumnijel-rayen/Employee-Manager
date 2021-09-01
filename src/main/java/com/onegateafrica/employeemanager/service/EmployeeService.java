0c7ea22781c467388e4a95d525f14e63e67157ac
boumnijel-rayen <boumnijelrayen9@gmail.com>
2021-09-01 19:19:33 +0200

    back end finished

--- src/main/java/com/onegateafrica/employeemanager/service/EmployeeService.java
+++ src/main/java/com/onegateafrica/employeemanager/service/EmployeeService.java
@@ -5,10 +5,12 @@ import com.onegateafrica.employeemanager.repo.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.UUID;
 
 @Service
+@Transactional
 public class EmployeeService {
     private final EmployeeRepo employeeRepo;
 
